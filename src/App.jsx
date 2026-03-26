e52a8d574df43a65e37967bee67d7f5859fbbe67
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-26 10:51:06 +0100

    implement search functionality for movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 
 
 function App() {
   
+  const [searchQuery, setSearchQuery] = useState('')
+  const [movieResults, setMovieResults] = useState([])
+  
   const API_KEY = import.meta.env.VITE_API_KEY
-  const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`
+  const api_url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchQuery}`
   console.log(api_url);
+  
+
+  function getMovieNames(e){
+    setSearchQuery(e.target.value)
+  }
+  console.log(searchQuery);
+
+
+  
+
   
 
   return (
     <>
-     
+     <input type="text" name="film_name" id="film_name" placeholder="insert movie name" value={searchQuery} onChange={getMovieNames}/>
+     <button onClick={searchMovies}>Search</button>
     </>
   )
 }
