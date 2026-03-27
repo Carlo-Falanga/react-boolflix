aa1d6c3560748fd1dfe923fc595d75201bfb0700
Carlo-Falanga <carlofalanga7@gmail.com>
2026-03-27 12:25:34 +0100

    refactor AppHeader component to improve code readability

--- src/assets/components/AppHeader.jsx
+++ src/assets/components/AppHeader.jsx
@@ -1,17 +1,22 @@
-import boolflixLogo from '../images/boolflix_logo_transparent.svg'
+import boolflixLogo from "../images/boolflix_logo_transparent.svg";
 
-export default function AppHeader({
-  searchQuery,
-  getMovieNames,
-  formControl,
-}) {
+const navListLinks = [
+  "Home",
+  "Series",
+  "Film",
+  "Games",
+  "New and popular",
+  "My list",
+  "Browse by language",
+];
+export default function AppHeader({ searchQuery, getMovieNames, formControl }) {
   return (
     <header>
-      <section className='navbar_section'>
+      <section className="navbar_section">
         <nav className="navbar navbar-expand-lg">
           <div className="container-fluid">
             <a className="navbar-brand" href="#">
-              <img className='nav_logo' src={boolflixLogo} alt="" />
+              <img className="nav_logo" src={boolflixLogo} alt="logo" />
             </a>
             <button
               className="navbar-toggler"
@@ -26,41 +31,17 @@ export default function AppHeader({
             </button>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item ">
-                  <a className="nav-link active text-white" aria-current="page" href="#">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-white" href="#">
-                    Series
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-white">
-                    Film
-                  </a>
-                </li>
-                 <li className="nav-item">
-                  <a className="nav-link text-white">
-                    Games
-                  </a>
-                </li>
-                 <li className="nav-item">
-                  <a className="nav-link text-white">
-                    New and popular
-                  </a>
-                </li>
-                 <li className="nav-item">
-                  <a className="nav-link text-white">
-                    My list
-                  </a>
-                </li>
-                 <li className="nav-item">
-                  <a className="nav-link text-white">
-                    Browse by language
-                  </a>
-                </li>
+                {navListLinks.map((link) => (
+                  <li className="nav-item" key={link}>
+                    <a
+                      className="nav-link active text-white"
+                      aria-current="page"
+                      href="#"
+                    >
+                      {link}
+                    </a>
+                  </li>
+                ))}
               </ul>
               <form className="d-flex" role="search" onSubmit={formControl}>
                 <input
@@ -71,10 +52,7 @@ export default function AppHeader({
                   value={searchQuery}
                   onChange={getMovieNames}
                 />
-                <button
-                  className="btn text-white d-flex"
-                  type="submit"
-                >
+                <button className="btn text-white d-flex" type="submit">
                   <i className="bi bi-search"></i>
                 </button>
               </form>
